69909975d2071da3a45babd1598d94421ac092a1
Shubham <shubham.takode@recykal.com>
2020-06-15 16:36:50 +0530

    Resolved update issue.

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,73 +1,50 @@
-import React, { Component } from 'react';
-import {  View, StyleSheet, Platform, ActivityIndicator } from 'react-native';
+import React, {Component, useEffect, useRef} from 'react';
+import {View, StyleSheet, Platform, ActivityIndicator} from 'react-native';
 import renderChart from './renderChart';
 import echarts from './echarts.min';
 import WebView from "react-native-webview";
 import getTpl from "./tpl" ;
 
-export default class App extends Component {
+const App = (props) => {
+    const webViewRef = useRef();
 
-  constructor(props) {
-    super(props);
-    this.setNewOption = this.setNewOption.bind(this);
-  }
+    useEffect(() => {
+        webViewRef.current.reload();
+    }, [props.option])
 
+    return <View style={{flex: 1, height: props.height || 400,}}>
+        <WebView
+            ref={webViewRef}
+            scrollEnabled={false}
+            injectedJavaScript={renderChart(props)}
+            originWhitelist={['*']}
+            source={{html: getTpl()}}
+            style={{
+                height: props.height || 400,
+                backgroundColor: props.backgroundColor || 'transparent'
+            }}
+            renderLoading={() => <ActivityIndicator
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    jusityContent: "space-around",
+                    flexWrap: "wrap",
+                    alignContent: "center",
+                }}
+                size="small"
+            />}
+            onError={(err) => {
+                props.onError && props.onError(err);
+            }}
+            onHttpError={(err) => {
+                props.onError && props.onError(err);
+            }}
+            onMessage={event => props.onPress ? props.onPress(JSON.parse(event.nativeEvent.data)) : null}
+        />
+    </View>
+};
 
-  componentWillReceiveProps(nextProps) {
-      //alert('here');
-      this.refs.chart.reload()
-    /*if(nextProps.option !== this.props.option) {
-      //this.refs.chart.reload();
-      if(Platform.OS === 'android'){
-        this.refs.chart.reload();
-      }else {
-        this.setNewOption(nextProps.option) ;
-      }
-    }*/
-  }
-  shouldComponentUpdate() {
-    return false ;
-  }
-
-  setNewOption(option) {
-    this.refs.chart.postMessage(JSON.stringify(option));
-  }
-
-  render() {
-    return (
-        <View style={{flex: 1, height: this.props.height || 400,}}>
-          <WebView
-              ref="chart"
-              scrollEnabled = {false}
-              injectedJavaScript = {renderChart(this.props)}
-              style={{
-                height: this.props.height || 400,
-                backgroundColor: this.props.backgroundColor || 'transparent'
-              }}
-              originWhitelist={['*']}
-              source={{html:getTpl() }}
-              renderLoading={() => <ActivityIndicator
-                  style={{
-                      position: "absolute",
-                      top: 0,
-                      left: 0,
-                      right: 0,
-                      bottom: 0,
-                      jusityContent: "space-around",
-                      flexWrap: "wrap",
-                      alignContent: "center",
-                  }}
-                  size="small"
-              />}
-              onError={(err) => {
-                  this.props.onError && this.props.onError(err);
-              }}
-              onHttpError={(err) => {
-                  this.props.onError && this.props.onError(err);
-              }}
-              onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
-          />
-        </View>
-    );
-  }
-}
+export default App;
